e29d31383f55ce8e393e8ca49f6d9c9361bcf3e3
tammyleextech <tammy.lee882@gmail.com>
2020-05-24 17:59:44 -0400

    Updates

---
+++ precache-manifest.62d654acec3759deff31e992cdab1de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647e48773bdd2efcd904aec1830098d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4461ab271727a771b79",
+    "url": "/static/css/main.0a5fec88.chunk.css"
+  },
+  {
+    "revision": "afd46756fa5ab1b2aed6",
+    "url": "/static/js/2.324d06ec.chunk.js"
+  },
+  {
+    "revision": "f4461ab271727a771b79",
+    "url": "/static/js/main.e56eeebc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
